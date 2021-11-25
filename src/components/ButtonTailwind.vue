ef16550be36943c868703fd2993e31af16188a16
kristjanjansen <kristjan.jansen@gmail.com>
2021-11-26 00:13:20 +0200

    feat: better button

--- src/components/ButtonTailwind.vue
+++ src/components/ButtonTailwind.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const buttonSizes = {
+  sm: "text-sm px-3 py-2",
+  md: "text-md px-4 py-3",
+  lg: "text-lg px-4 py-4",
+};
+
+type ButtonSize = keyof typeof buttonSizes;
+
+const { size = "md" } = defineProps<{ size?: ButtonSize }>();
+</script>
 <template>
   <button
     class="
@@ -6,8 +17,8 @@
       hover:bg-gray-50
       text-gray-500
       font-bold
-      p-4
     "
+    :class="buttonSizes[size]"
   >
     <slot />
   </button>
