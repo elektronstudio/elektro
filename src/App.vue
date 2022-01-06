7268570fc7e8d21e69439463f431fef13fa45189
kristjanjansen <kristjan.jansen@gmail.com>
2022-01-06 13:38:19 +0200

    fix: import name

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { EDebug } from "./lib";
+// EDebug is an internal component and not exported in "./lib"
+import EDebug from "./components/EDebug.vue";
 </script>
 <template>
   <router-view />
