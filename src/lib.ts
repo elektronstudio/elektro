c6a34fe82cf14af46e9be4ded72746baac793f39
Sven Raju <sven@wwwstuudio.ee>
2022-01-10 16:58:33 +0100

    feat: add EProjectCard component

--- src/lib.ts
+++ src/lib.ts
@@ -4,6 +4,7 @@ export { default as EDebug } from "./components/EDebug.vue";
 export { default as ENav } from "./components/ENav.vue";
 export { default as ENavButton } from "./components/ENavButton.vue";
 export { default as EContent } from "./components/EContent.vue";
+export { default as EProjectCard } from "./components/EProjectCard.vue";
 
 import "./styles/vars.css";
 import "./styles/reset.css";
