b9697ca28afef5aa15cc6e8f3fa50eaa278ea373
Sven Raju <sven@wwwstuudio.ee>
2022-01-12 15:14:56 +0200

    feat: add EScheduleEvent component

--- src/components/EScheduleEvent.vue
+++ src/components/EScheduleEvent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import EButton from "./EButton.vue";
+import EContent from "./EContent.vue";
 
 type Props = {
   title: string;
@@ -13,17 +14,52 @@ type Props = {
 const { title, description, events } = defineProps<Props>();
 </script>
 <template>
-  <article>
+  <article class="EscheduleEvent">
     <h3>{{ title }}</h3>
-    <div v-if="description" v-html="description" />
-    <template v-if="events">
-      <div>
-        <div v-for="event in events">
-          <time :datetime="event.startTime">{{ event.startTime }}</time>
-          <EButton size="xs" el="a">+ Lisa kalendrisse</EButton>
-          <EButton el="a" size="xs" color="accent">→ OSTA PILET</EButton>
-        </div>
+    <EContent v-if="description" :content="description" />
+    <div v-if="events">
+      <!-- Should we make this a separate component? -->
+      <div v-for="event in events" className="instance">
+        <time :datetime="event.startTime">{{ event.startTime }}</time>
+        <EButton size="xs" el="a" color="transparent"
+          >+ Lisa kalendrisse</EButton
+        >
+        <EButton el="a" size="xs" color="accent">→ OSTA PILET</EButton>
       </div>
-    </template>
+    </div>
   </article>
 </template>
+
+<style scoped>
+.EscheduleEvent {
+  display: flex;
+  flex-direction: column;
+  /* TODO: Remove this */
+  max-width: 800px;
+}
+.EscheduleEvent h3,
+.EscheduleEvent article {
+  color: var(--gray-300);
+  max-width: 60ch;
+  margin-bottom: var(--p-3);
+}
+
+.EscheduleEvent h3 {
+  font-size: var(--text-3xl);
+}
+.instance {
+  display: flex;
+  justify-content: space-between;
+  padding: var(--p-3) 0;
+}
+.instance:not(:last-child) {
+  border-bottom: 1px solid var(--gray-300);
+}
+.instance > article,
+.instance > *:last-child {
+  margin-left: var(--p-3);
+}
+.instance time {
+  margin-right: auto;
+}
+</style>
