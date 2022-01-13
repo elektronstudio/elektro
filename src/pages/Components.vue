4207f48ec15f96366e8614133938867dddcc963c
Sven Raju <sven@wwwstuudio.ee>
2022-01-13 16:04:38 +0200

    feat: add width constraint for components

--- src/pages/Components.vue
+++ src/pages/Components.vue
@@ -8,7 +8,6 @@ import {
   EProjectCard,
   EScheduleEvent,
   EEventInstance,
-  EBoxWrapper,
 } from "../lib";
 const navItems = [
   {
@@ -63,116 +62,106 @@ const scheduleEvents = [
 </script>
 
 <template>
-  <ETitle size="lg">Components</ETitle>
+  <div class="Components">
+    <ETitle size="lg">Components</ETitle>
 
-  <br />
-  <pre>ETitle</pre>
-  <ETitle size="lg">Title lg</ETitle>
-  <ETitle size="md">Title md</ETitle>
-  <ETitle size="sm">Title sm</ETitle>
+    <br />
+    <pre>ETitle</pre>
+    <ETitle size="lg">Title lg</ETitle>
+    <ETitle size="md">Title md</ETitle>
+    <ETitle size="sm">Title sm</ETitle>
 
-  <br />
-  <pre>EContent</pre>
-  <EContent>
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <h5>Heading 5</h5>
-    <h6>Heading 6</h6>
-    <p>
-      <a href="https://elektron.art" target="_blank">eˉlektron</a> on
-      transdistsiplinaarne etendustkunsti platvorm, mis liidab kaasaegse kunsti,
-      teaduse ja tehnoloogia otsingulisi tegevusi.<br />Ootamatud kunstivormid,
-      lavastused, tekstid, taskuhääling, eksperimendid.
-    </p>
-    <p>
-      <b>e‾lektron</b> on moodustatud MIMprojecti ja Kanuti Gildi SAALi
-      koostöös. Rahaline toetaja on Eesti kultuuriministeerium.
-    </p>
-    <p>
-      <i>eˉlektron</i> is a halfway virtual, halfway physical platform that
-      connects exploratory activities of performing arts and science.<br />Unexpected
-      art forms, performances, texts, podcast, experiments.
-    </p>
-    <p>
-      e‾lektron is realised in cooperation with MIMproject and Kanuti Gildi
-      SAAL. Financial supporter is Estonian Ministry of Culture.
-    </p>
-    <p><small>Kes on eˉlektron? / Who is eˉlektron?</small></p>
-    <ul>
-      <li>Unordered list item</li>
-      <li>Unordered list item</li>
-      <li>Unordered list item</li>
-      <li>Unordered list item</li>
-      <li>Unordered list item</li>
-    </ul>
-    <ol>
-      <li>Ordered list item</li>
-      <li>Ordered list item</li>
-      <li>Ordered list item</li>
-      <li>Ordered list item</li>
-      <li>Ordered list item</li>
-    </ol>
-    <blockquote>
-      Marshall Stay is a multi-disciplinary performance-maker from Australia.
-      Shortly after graduating from the Western Australian Academy of Performing
-      Arts in 2018, he co-created and directed a number of award-nominated
-      theatre pieces, as well as continuing his practice as a freelance
-      photographer and videographer within the artistic community.
-    </blockquote>
-  </EContent>
+    <br />
+    <pre>EContent</pre>
+    <EContent>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>
+        <a href="https://elektron.art" target="_blank">eˉlektron</a> on
+        transdistsiplinaarne etendustkunsti platvorm, mis liidab kaasaegse
+        kunsti, teaduse ja tehnoloogia otsingulisi tegevusi.<br />Ootamatud
+        kunstivormid, lavastused, tekstid, taskuhääling, eksperimendid.
+      </p>
+      <p>
+        <b>e‾lektron</b> on moodustatud MIMprojecti ja Kanuti Gildi SAALi
+        koostöös. Rahaline toetaja on Eesti kultuuriministeerium.
+      </p>
+      <p>
+        <i>eˉlektron</i> is a halfway virtual, halfway physical platform that
+        connects exploratory activities of performing arts and science.<br />Unexpected
+        art forms, performances, texts, podcast, experiments.
+      </p>
+      <p>
+        e‾lektron is realised in cooperation with MIMproject and Kanuti Gildi
+        SAAL. Financial supporter is Estonian Ministry of Culture.
+      </p>
+      <p><small>Kes on eˉlektron? / Who is eˉlektron?</small></p>
+      <ul>
+        <li>Unordered list item</li>
+        <li>Unordered list item</li>
+        <li>Unordered list item</li>
+        <li>Unordered list item</li>
+        <li>Unordered list item</li>
+      </ul>
+      <ol>
+        <li>Ordered list item</li>
+        <li>Ordered list item</li>
+        <li>Ordered list item</li>
+        <li>Ordered list item</li>
+        <li>Ordered list item</li>
+      </ol>
+      <blockquote>
+        Marshall Stay is a multi-disciplinary performance-maker from Australia.
+        Shortly after graduating from the Western Australian Academy of
+        Performing Arts in 2018, he co-created and directed a number of
+        award-nominated theatre pieces, as well as continuing his practice as a
+        freelance photographer and videographer within the artistic community.
+      </blockquote>
+    </EContent>
 
-  <br />
-  <pre>EButton</pre>
-  <div style="display: flex; align-items: flex-start; gap: var(--gap-4)">
-    <EButton size="lg">Button lg</EButton>
-    <EButton size="md">Button md as a element</EButton>
-    <EButton size="sm" color="accent">Button sm accent colored</EButton>
-    <EButton size="xs" color="accent">Button xs accent colored</EButton>
-    <EButton size="xs" color="gray">Button xs gray colored</EButton>
-  </div>
+    <br />
+    <pre>EButton</pre>
+    <div style="display: flex; align-items: flex-start; gap: var(--gap-4)">
+      <EButton size="lg">Button lg</EButton>
+      <EButton size="md">Button md as a element</EButton>
+      <EButton size="sm" color="accent">Button sm accent colored</EButton>
+      <EButton size="xs" color="accent">Button xs accent colored</EButton>
+      <EButton size="xs" color="gray">Button xs gray colored</EButton>
+    </div>
 
-  <br />
-  <pre>ENav</pre>
-  <ENav :navItems="navItems" />
+    <br />
+    <pre>ENav</pre>
+    <ENav :navItems="navItems" />
 
-  <br />
-  <pre>ENavButton</pre>
-  <ENavButton>About us</ENavButton>
+    <br />
+    <pre>ENavButton</pre>
+    <ENavButton>About us</ENavButton>
 
-  <br />
-  <pre>EProjectCard</pre>
-  <EProjectCard
-    v-for="project in projects"
-    :title="project.title"
-    :author="project.author"
-    :thumbnail="project.thumbnail"
-    :events="project.events"
-  />
+    <br />
+    <pre>EProjectCard</pre>
+    <EProjectCard
+      v-for="project in projects"
+      :title="project.title"
+      :author="project.author"
+      :thumbnail="project.thumbnail"
+      :events="project.events"
+    />
 
-  <br />
-  <pre>EScheduleEvent</pre>
-  <EScheduleEvent
-    v-for="event in scheduleEvents"
-    :title="event.title"
-    :description="event.description"
-    :events="event.events"
-  />
+    <br />
+    <pre>EScheduleEvent</pre>
+    <EScheduleEvent
+      v-for="event in scheduleEvents"
+      :title="event.title"
+      :description="event.description"
+      :events="event.events"
+    />
 
-  <br />
-  <pre>EEventInstance</pre>
-  <EEventInstance
-    v-for="eventInstance in scheduleEvents[0].events"
-    :startTime="eventInstance.startTime"
-    :endTime="eventInstance.endTime"
-    :ticketUrl="eventInstance.ticketUrl"
-    layout="vertical"
-  />
-
-  <br />
-  <pre>EBoxWrapper</pre>
-  <EBoxWrapper>
+    <br />
+    <pre>EEventInstance</pre>
     <EEventInstance
       v-for="eventInstance in scheduleEvents[0].events"
       :startTime="eventInstance.startTime"
@@ -180,5 +169,12 @@ const scheduleEvents = [
       :ticketUrl="eventInstance.ticketUrl"
       layout="vertical"
     />
-  </EBoxWrapper>
+  </div>
 </template>
+
+<style scoped>
+.Components {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
