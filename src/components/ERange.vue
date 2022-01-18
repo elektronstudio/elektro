81e16e245100a0d4e5cc6e0e550f061cf3db48ac
Sven Raju <sven@wwwstuudio.ee>
2022-01-18 16:30:02 +0200

    feat: update ERange styles

--- src/components/ERange.vue
+++ src/components/ERange.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import ELabel from "./ELabel.vue";
 import EInput from "./EInput.vue";
+
 type Props = {
   label?: string;
   value?: number;
@@ -20,6 +22,10 @@ const {
   showMinMax,
   showOutput,
 } = defineProps<Props>();
+
+const backgroundSize = computed(
+  () => ((value - min) * 100) / (max - min) + "% 100%",
+);
 </script>
 <template>
   <div class="ERange">
@@ -33,6 +39,7 @@ const {
       :max="max"
       :step="step"
       v-model="value"
+      :style="{ backgroundSize }"
     />
     <span v-if="showMinMax && !showOutput" class="max">{{ max }}</span>
     <EInput
@@ -49,7 +56,6 @@ const {
 .ERange {
   font-family: var(--font-mono);
   font-size: var(--text-xs);
-  accent-color: var(--accent);
   width: 100%;
   display: flex;
   align-items: center;
@@ -61,10 +67,84 @@ const {
   margin-left: var(--m-2);
 }
 
-.ERange input {
+input[type="range"] {
+  appearance: none;
   width: 100%;
+  height: var(--h-px);
+  background-color: var(--gray-300);
+  background-image: linear-gradient(var(--accent), var(--accent));
+  background-repeat: no-repeat;
 }
 
+/* Input Thumb */
+input[type="range"]::-webkit-slider-thumb {
+  appearance: none;
+  height: var(--w-4);
+  width: var(--h-4);
+  border-radius: 50%;
+  background: var(--fg);
+  cursor: ew-resize;
+  box-shadow: none;
+  border: none;
+  outline: none;
+  transition: background 0.3s ease-in-out;
+}
+
+input[type="range"]::-moz-range-thumb {
+  appearance: none;
+  height: var(--w-4);
+  width: var(--h-4);
+  border-radius: 50%;
+  background: var(--fg);
+  cursor: ew-resize;
+  box-shadow: none;
+  border: none;
+  outline: none;
+  transition: background 0.3s ease-in-out;
+}
+
+input[type="range"]::-ms-thumb {
+  appearance: none;
+  height: var(--w-4);
+  width: var(--h-4);
+  border-radius: 50%;
+  background: var(--fg);
+  cursor: ew-resize;
+  box-shadow: none;
+  border: none;
+  outline: none;
+  transition: background 0.2s ease-in-out;
+}
+
+input[type="range"]::-webkit-slider-thumb:hover {
+  background: var(--gray-300);
+}
+input[type="range"]::-moz-range-thumb:hover {
+  background: var(--gray-300);
+}
+input[type="range"]::-ms-thumb:hover {
+  background: var(--gray-300);
+}
+
+/* Input Track */
+input[type="range"]::-webkit-slider-runnable-track {
+  appearance: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
+input[type="range"]::-moz-range-track {
+  appearance: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
+input[type="range"]::-ms-track {
+  appearance: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
 .ERange .EInput {
   margin-left: var(--m-4);
   max-width: 4rem;
