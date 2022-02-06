9dc1e9f1cf08b982eabca79d8dcbdf0c7244894a
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-06 20:44:40 +0200

    cleanup

--- src/pages/Videostream.vue
+++ src/pages/Videostream.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { useVideostream } from "../utils/videostream";
 import ETitle from "../components/ETitle.vue";
-const url =
-  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+import EFormText from "../components/EFormText.vue";
+import { ref } from "vue";
+//const defaultUrl =
+//("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8");
+
+const defaultUrl = "https://cloudflare.tv/hls/live.m3u8";
+
+const url = ref(defaultUrl);
 const { videoRef, status, width, height } = useVideostream(url);
 </script>
 
 <template>
   <div class="Videostream">
+    <EFormText v-model="url" label="HLS URL" />
     <ETitle>Videostream</ETitle>
     <video
       muted
