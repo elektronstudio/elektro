16bb3eb747a053d1b7bcb6ae3e171938c46dd3cb
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-02-07 18:19:22 +0200

    feat: dates, take II (#40)

---
+++ src/pages/date.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { sub, add } from "date-fns";
+import ETitle from "../components/ETitle.vue";
+import EBox from "../components/EBox.vue";
+import { useRange } from "../utils/date";
+
+const ranges = [
+  {
+    start_at: sub(new Date(), { hours: 2 }),
+    end_at: sub(new Date(), { hours: 1 }),
+  },
+  {
+    start_at: sub(new Date(), { minutes: 10 }),
+    end_at: add(new Date(), { hours: 1 }),
+  },
+  {
+    start_at: add(new Date(), { days: 2 }),
+    end_at: add(new Date(), { days: 2, hours: 2 }),
+  },
+].map((range) => ({
+  ...range,
+  ...useRange(range.start_at, range.end_at),
+}));
+</script>
+
+<template>
+  <div class="Date">
+    <ETitle size="lg">Date formatting</ETitle>
+    <EBox v-for="range in ranges">
+      <pre>{{ range }}</pre>
+    </EBox>
+  </div>
+</template>
+<style scoped>
+.Date {
+  display: grid;
+  gap: var(--gap-4);
+  padding: var(--p-4);
+}
+</style>
