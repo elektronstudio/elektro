00dea9f558220f252c80e2da0fda58c9dfaba407
sven-ra <mail@svenraju.net>
2022-02-09 16:33:03 +0200

    feat: restructure about page blocks

--- src/components/ERelatedPageCard.vue
+++ src/components/ERelatedPageCard.vue
@@ -4,18 +4,21 @@ import ETitle from "../components/ETitle.vue";
 import EContent from "../components/EContent.vue";
 
 type Props = {
-  layout?: "columns1" | "columns2" | "columns3" | "columns4";
   title?: string;
-  content?: string;
 };
 
-const { layout, title, content } = defineProps<Props>();
+const { title } = defineProps<Props>();
 </script>
 <template>
-  <!-- @TODO: minimum aspect ratio is probably needed -->
-  <EBox :class="layout">
+  <EBox color="accent" ratio="1 / 1" layout="columns1">
     <ETitle v-if="title" el="h3" size="lg" :title="title" />
-    <EContent v-if="content" :content="content" />
+    <!-- @TODO: add routing system -->
+    <!-- <RouterLink :to="#"> -->
+    <EContent>
+      <!-- @TODO: Add translation -->
+      Read more
+    </EContent>
+    <!-- </RouterLink> -->
   </EBox>
 </template>
 
@@ -24,8 +27,5 @@ const { layout, title, content } = defineProps<Props>();
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-}
-.EBox h3 {
-  margin-bottom: var(--m-8);
 }
 </style>
