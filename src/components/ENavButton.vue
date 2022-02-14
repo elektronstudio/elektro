4e51cc2a17601b4962a0ead722b5541880c6c367
sven-ra <mail@svenraju.net>
2022-02-14 13:42:27 +0200

    feat: add ENavButton variants

--- src/components/ENavButton.vue
+++ src/components/ENavButton.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+type Props = {
+  variant?: "outline" | "transparent";
+};
+
+const { variant = "transparent" } = defineProps<Props>();
+</script>
+
 <template>
-  <a class="ENavButton">
+  <a class="ENavButton" :class="variant">
     <slot />
   </a>
 </template>
@@ -7,12 +15,19 @@
 <style scoped>
 .ENavButton {
   display: inline-flex;
+  align-items: center;
   padding: var(--p-1);
-  border: var(--border-DEFAULT) solid var(--gray-500);
   font-family: var(--font-mono);
   font-size: var(--text-xs);
   text-transform: uppercase;
   color: var(--gray-300);
+}
+/* @TODO: sync with icon system */
+.ENavButton.outline {
+  border: var(--border-DEFAULT) solid var(--gray-500);
+}
+.ENavButton :deep(svg) {
+  margin-right: var(--m-1);
 }
 .ENavButton:not(:first-child) {
   margin-left: calc(var(--border-DEFAULT) * -1);
