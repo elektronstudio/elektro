ceff2fb0e7d86a1b09e232f2cfd9142e2fd9eae4
sven-ra <mail@svenraju.net>
2022-02-14 20:08:55 +0200

    feat: add EWindowBorder component (#47)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Debug from "./components/Debug.vue";
+import EWindowBorder from "./components/EWindowBorder.vue";
 </script>
 
 <template>
+  <EWindowBorder />
   <router-view />
   <Debug />
 </template>
