1424a0c82ccbd628235a27efca3b9d3a3a72742d
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-17 14:28:27 +0200

    cleanup

--- vite.config.app.ts
+++ vite.config.app.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
-import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 export default defineConfig({
   plugins: [
     Vue({
       reactivityTransform: true,
     }),
-    ViteWebfontDownload([
-      "https://fonts.googleapis.com/css2?family=Azeret+Mono&family=Cousine&display=swap",
-    ]),
     Pages(),
   ],
 });
--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import path from "path";
 import Vue from "@vitejs/plugin-vue";
-import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 export default defineConfig({
   plugins: [
     Vue({
       reactivityTransform: true,
     }),
-    ViteWebfontDownload([
-      "https://fonts.googleapis.com/css2?family=Azeret+Mono&family=Cousine&display=swap",
-    ]),
   ],
   build: {
     lib: {
