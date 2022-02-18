21452ba9b3460629b169df47a25aa31a68923370
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-02-18 13:51:39 +0200

    feat: EStack (#55)

---
+++ src/pages/stack.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { EStack, EBox, EButton } from "../lib";
+import ETitle from "../components/ETitle.vue";
+</script>
+<template>
+  <EStack class="Stack">
+    <ETitle>Vertical</ETitle>
+    <EStack>
+      <EBox>Hello</EBox>
+      <EBox>World</EBox>
+      <div><EButton>Hello</EButton></div>
+    </EStack>
+    <br />
+    <ETitle>Horizontal</ETitle>
+    <EStack layout="horizontal" :cols="3">
+      <EBox>Hello</EBox>
+      <EBox>World</EBox>
+      <div><EButton>Hello</EButton></div>
+    </EStack>
+  </EStack>
+</template>
+
+<style scoped>
+.Stack {
+  padding: var(--p-4);
+}
+</style>
