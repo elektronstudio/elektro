bbf3db1f5314fb9f10c39289dc535fdda254c7e9
sven-ra <mail@svenraju.net>
2022-02-21 17:38:23 +0200

    feat: update EImageSlider responsive layouts

--- src/components/EImageSlider.vue
+++ src/components/EImageSlider.vue
@@ -46,7 +46,7 @@ const { images } = defineProps<Props>();
 }
 
 .EImageSlider figcaption {
-  width: 16rem;
+  max-width: 16rem;
   font-size: var(--text-xs);
   font-family: var(--font-mono);
   line-height: var(--line-height-xs);
@@ -55,8 +55,18 @@ const { images } = defineProps<Props>();
 }
 
 .EImageSlider .EImage {
-  height: 40rem;
+  height: 70vw;
   width: auto;
   border-radius: var(--rounded-4xl);
+}
+@media only screen and (min-width: 600px) {
+  .EImageSlider .EImage {
+    height: 40vw;
+  }
+}
+@media only screen and (min-width: 1000px) {
+  .EImageSlider .EImage {
+    height: 30vw;
+  }
 }
 </style>
