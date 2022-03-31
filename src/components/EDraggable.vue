5973e075e99aabd97b7f28f5930c1712ea6c5fd5
sven-ra <mail@svenraju.net>
2022-03-31 11:33:20 +0200

    feat: add minise and maximise icons

--- src/components/EDraggable.vue
+++ src/components/EDraggable.vue
@@ -119,7 +119,21 @@ function findCoordinates(el: Element, done: () => void) {
             })
           "
         >
-          M
+          <!-- @TODO: add icons system to Elektro -->
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.5 3.04999C11.7485 3.04999 11.95 3.25146 11.95 3.49999V7.49999C11.95 7.74852 11.7485 7.94999 11.5 7.94999C11.2515 7.94999 11.05 7.74852 11.05 7.49999V4.58639L4.58638 11.05H7.49999C7.74852 11.05 7.94999 11.2515 7.94999 11.5C7.94999 11.7485 7.74852 11.95 7.49999 11.95L3.49999 11.95C3.38064 11.95 3.26618 11.9026 3.18179 11.8182C3.0974 11.7338 3.04999 11.6193 3.04999 11.5L3.04999 7.49999C3.04999 7.25146 3.25146 7.04999 3.49999 7.04999C3.74852 7.04999 3.94999 7.25146 3.94999 7.49999L3.94999 10.4136L10.4136 3.94999L7.49999 3.94999C7.25146 3.94999 7.04999 3.74852 7.04999 3.49999C7.04999 3.25146 7.25146 3.04999 7.49999 3.04999L11.5 3.04999Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </button>
         <button
           @click.stop="
@@ -130,7 +144,22 @@ function findCoordinates(el: Element, done: () => void) {
             })
           "
         >
-          ⅹ
+          <!-- @TODO: add icons system to Elektro -->
+
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.25 7.5C1.25 7.22386 1.47386 7 1.75 7H13.25C13.5261 7 13.75 7.22386 13.75 7.5C13.75 7.77614 13.5261 8 13.25 8H1.75C1.47386 8 1.25 7.77614 1.25 7.5Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </button>
       </nav>
       <div v-if="!draggable.isMaximised" class="titleBar" ref="draggableRef">
@@ -213,11 +242,14 @@ function findCoordinates(el: Element, done: () => void) {
   position: absolute;
   top: 0;
   right: 0;
+  display: flex;
 }
 
 .topBarNav button {
   height: var(--h-6);
   width: var(--w-6);
+  display: grid;
+  place-content: center;
 }
 .topBarNav button:hover {
   background-color: var(--gray-300);
