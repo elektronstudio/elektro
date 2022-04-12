e34c057d66a3c92a44a4c2e5332ef2f6542665e9
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-04-12 21:19:55 +0300

    cleanup

--- src/pages/draggablechat.vue
+++ src/pages/draggablechat.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { debouncedWatch, useDraggable } from "@vueuse/core";
 import { useMessage, safeJsonParse, userId, userName } from "../utils";
 import type { Message } from "../utils";
 
@@ -35,6 +36,39 @@ ws.addEventListener("message", ({ data }) => {
   }
 });
 
+const el = ref<HTMLElement | null>(null);
+
+const { x, y, style } = useDraggable(el, {
+  initialValue: { x: 100, y: 100 },
+});
+
+const USER_UPDATE_RATE = 50; // ms
+
+const otherUsers = computed(() =>
+  users.value.filter((u) => u.userId !== userId.value),
+);
+
+debouncedWatch(
+  [x, y],
+  () => {
+    const message: Message = {
+      type: "USER",
+      channel,
+      userId: userId.value,
+      userName: userName.value,
+      value: {
+        x: x.value,
+        y: y.value,
+      },
+    };
+    sendMessage(message);
+  },
+  {
+    immediate: true,
+    debounce: USER_UPDATE_RATE,
+  },
+);
+
 const onDrag = () => {
   const message: Message = {
     type: "USER",
@@ -53,6 +87,28 @@ const onDrag = () => {
 <template>
   <div style="padding: var(--gap-4)">
     <button @click="onDrag">Drag</button>
-    <pre>{{ users }}</pre>
+    <pre style="pointer-events: none">{{ users }}</pre>
   </div>
+  <div
+    v-for="user in otherUsers"
+    :style="{ top: user.y + 'px', left: user.x + 'px' }"
+    style="
+      position: fixed;
+      width: 25px;
+      height: 25px;
+      background: rgba(255, 255, 255, 0.8);
+      border-radius: 10000px;
+    "
+  />
+  <div
+    ref="el"
+    :style="style"
+    style="
+      position: fixed;
+      width: 25px;
+      height: 25px;
+      background: red;
+      border-radius: 10000px;
+    "
+  />
 </template>
