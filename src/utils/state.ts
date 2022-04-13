82b9ce845fda9030523491812f31d3c0dbc36c0b
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-04-13 09:25:51 +0300

    fix types

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,5 +1,11 @@
-import { useSessionStorage } from "@vueuse/core";
+import { RemovableRef, useSessionStorage } from "@vueuse/core";
 import { randomString } from ".";
 
-export const userId = useSessionStorage("elektron_user_id", randomString());
-export const userName = useSessionStorage("elektron_user_name", randomString());
+export const userId: RemovableRef<string> = useSessionStorage(
+  "elektron_user_id",
+  randomString(),
+);
+export const userName: RemovableRef<string> = useSessionStorage(
+  "elektron_user_name",
+  randomString(),
+);
