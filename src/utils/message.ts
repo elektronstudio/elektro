d73130e8dfdbae41a3236736c91641e91a120ea7
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-04-13 09:21:26 +0300

    fix

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -29,7 +29,6 @@ export function formatMessage(message: Object): string {
 export function useMessage() {
   const ws = new ReconnectingWebsocket(config.wsUrl as UrlProvider);
   const messages = ref<Message[]>([]);
-  const newMessage = ref<Message | null>(null);
 
   fetch(config.messagesUrl as RequestInfo)
     .then((res) => res.json())
@@ -53,5 +52,5 @@ export function useMessage() {
 
   const sendMessage = (message: Message) => ws.send(formatMessage(message));
 
-  return { ws, messages, newMessage, sendMessage };
+  return { ws, messages, sendMessage };
 }
